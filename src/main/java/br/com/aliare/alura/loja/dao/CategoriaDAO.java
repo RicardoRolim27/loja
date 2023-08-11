30b8f1373d4bfeb122424bee0af3cec0ffef4d73
Ricardo Rolim de Oliveira <ricardorolim27@gmail.com>
2023-08-11 11:55:00 -0300

    atualizações gerais
    Criado métodos atualizar e remover na classe CategoriaDAO

--- src/main/java/br/com/aliare/alura/loja/dao/CategoriaDAO.java
+++ src/main/java/br/com/aliare/alura/loja/dao/CategoriaDAO.java
@@ -16,5 +16,14 @@ private EntityManager entityManager;
 	public void cadastrar(Categoria categoria) {
 		this.entityManager.persist(categoria);
 	}
+	
+	public void atualizar(Categoria categoria) {
+		this.entityManager.merge(categoria);
+	}
+	
+	public void remover(Categoria categoria) {
+		categoria = entityManager.merge(categoria);
+		this.entityManager.remove(categoria);
+	}
 
 }
